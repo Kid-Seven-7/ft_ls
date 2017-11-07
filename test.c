84ba3700398bfe885c75e40f2e066ec7b8078582
Joseph NGOMA <jngoma@e4r19p2.wethinkcode.co.za>
2017-11-07 09:40:56 +0200

    Making progress. Still need to cook up the 2d arrays

---
+++ test.c
@@ -0,0 +1,30 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+  DIR             *d;
+  struct dirent   *dir;
+  int             i;
+  char            *str;
+  //int             hold;
+  //char            **names;
+
+  d = opendir(".");
+  i = -1;
+  if (d)
+  {
+    while ((dir = readdir(d)) != NULL)
+    {
+      //TODO place all the names into a 2d array for sorting
+      i++;
+      str = dir->d_name;
+      printf("%s\ni is %i\n", str, i);
+      //printf("%i is %s which is %lu chars long\n", i, dir->d_name, strlen(dir->d_name));
+    }
+    closedir(d);
+  }
+  return (0);
+}

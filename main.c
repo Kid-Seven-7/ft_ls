b7a59d74d5a2a1582a66d7a804dfc75e7ca6ce58
Joseph NGOMA <jngoma@e4r19p2.wethinkcode.co.za>
2017-11-20 15:43:52 +0200

    added a main

--- main.c
+++ main.c
@@ -6,14 +6,23 @@
 /*   By: jngoma <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/20 11:56:38 by jngoma            #+#    #+#             */
-/*   Updated: 2017/11/20 12:07:32 by jngoma           ###   ########.fr       */
+/*   Updated: 2017/11/20 13:19:18 by jngoma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int main(void)
+int main(int ac, char **av)
 {
+	if (ac > 1)
+	{
+		printf("%i\n", arg_check(ac, av));
+		if (arg_check(ac, av) == 0)
+			read_into_dir();
+		else
+			ft_putendl("invalid");
+	}
+	else
 	read_into_dir();
 	return (0);
 }

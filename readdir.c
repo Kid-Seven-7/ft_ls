c74e80233465f9e3f66a0b756e2a100b444f3ca7
Kid-Seven-7 <testing.jo.wtc@gmail.com>
2017-11-21 19:49:02 +0200

    almost done

--- readdir.c
+++ readdir.c
@@ -12,7 +12,10 @@
 
 #include "ft_ls.h"
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <time.h>
+#include <pwd.h>
+#include <grp.h>
 
 void	sub(char * str)
 {
@@ -56,12 +59,15 @@ void	read_into_dir(void)
 {
 	DIR				*d;
 	struct dirent	*dir;
+	struct passwd	*user;
+	struct group	*grp;
 	t_list			*head;
 
 	struct stat		foo;
 
 	d = opendir(".");
 	head = NULL;
+	user = NULL;
 	if (d)
 	{
 		while ((dir = readdir(d)) != NULL)
@@ -83,11 +89,25 @@ void	read_into_dir(void)
 			printf("time access: %s", ctime(&foo.st_atime));
 			printf("time modified: %s", ctime(&foo.st_mtime));
 			printf("time status: %s", ctime(&foo.st_ctime));
-			printf("uid: %u\n", foo.st_uid);
-			printf("gid: %u\n", foo.st_gid);
-			printf("size: %lld\n", foo.st_size);
-			printf("link: %hu\n", foo.st_nlink);
-			printf("mode: %hu\n", foo.st_mode);
+			//printf("uid: %u\n", foo.st_uid);
+			user = getpwuid(foo.st_uid);
+			printf("uid: %s\n",user->pw_name);
+			//printf("gid: %u\n", foo.st_gid);
+			grp = getgrgid(foo.st_gid);
+			printf("uid: %s\n",grp->gr_name);
+			printf("size: %ld\n", foo.st_size);
+			printf("link: %lu\n", foo.st_nlink);
+			//printf("mode: %u\n", foo.st_mode);
+			printf((S_ISDIR(foo.st_mode)) ? "d" : "-");
+			printf((foo.st_mode & S_IRUSR) ? "r" : "-");
+			printf((foo.st_mode & S_IWUSR) ? "w" : "-");
+			printf((foo.st_mode & S_IXUSR) ? "x" : "-");
+			printf((foo.st_mode & S_IRGRP) ? "r" : "-");
+			printf((foo.st_mode & S_IWGRP) ? "w" : "-");
+			printf((foo.st_mode & S_IXGRP) ? "x" : "-");
+			printf((foo.st_mode & S_IROTH) ? "r" : "-");
+			printf((foo.st_mode & S_IWOTH) ? "w" : "-");
+			printf((foo.st_mode & S_IXOTH) ? "x" : "-");
 			printf("\n");
 				}
 			}
@@ -99,11 +119,25 @@ void	read_into_dir(void)
 			printf("time access: %s", ctime(&foo.st_atime));
 			printf("time modified: %s", ctime(&foo.st_mtime));
 			printf("time status: %s", ctime(&foo.st_ctime));
-			printf("uid: %u\n", foo.st_uid);
-			printf("gid: %u\n", foo.st_gid);
-			printf("size: %lld\n", foo.st_size);
-			printf("link: %hu\n", foo.st_nlink);
-			printf("mode: %hu\n", foo.st_mode);
+			//printf("uid: %u\n", foo.st_uid);
+			user = getpwuid(foo.st_uid);
+			printf("uid: %s\n",user->pw_name);
+			//printf("gid: %u\n", foo.st_gid);
+			grp = getgrgid(foo.st_gid);
+			printf("uid: %s\n",grp->gr_name);
+			printf("size: %ld\n", foo.st_size);
+			printf("link: %lu\n", foo.st_nlink);
+			//printf("mode: %u\n", foo.st_mode);
+			printf((S_ISDIR(foo.st_mode)) ? "d" : "-");
+			printf((foo.st_mode & S_IRUSR) ? "r" : "-");
+			printf((foo.st_mode & S_IWUSR) ? "w" : "-");
+			printf((foo.st_mode & S_IXUSR) ? "x" : "-");
+			printf((foo.st_mode & S_IRGRP) ? "r" : "-");
+			printf((foo.st_mode & S_IWGRP) ? "w" : "-");
+			printf((foo.st_mode & S_IXGRP) ? "x" : "-");
+			printf((foo.st_mode & S_IROTH) ? "r" : "-");
+			printf((foo.st_mode & S_IWOTH) ? "w" : "-");
+			printf((foo.st_mode & S_IXOTH) ? "x" : "-");
 			printf("\n");
 			//printf("time %ld\n", foo.st_atime);
 			//printf("time %ld\n", foo.st_atime);

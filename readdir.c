fb7dd8d7727fc9a24b4fb053174248dfd806a92c
Joseph NGOMA <jngoma@e4r23p1.wethinkcode.co.za>
2017-11-21 15:46:44 +0200

    basics sorted

--- readdir.c
+++ readdir.c
@@ -6,11 +6,13 @@
 /*   By: jngoma <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/13 11:06:00 by jngoma            #+#    #+#             */
-/*   Updated: 2017/11/21 14:11:44 by jngoma           ###   ########.fr       */
+/*   Updated: 2017/11/21 15:44:16 by jngoma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+#include <sys/stat.h>
+#include <time.h>
 
 void	sub(char * str)
 {
@@ -56,6 +58,8 @@ void	read_into_dir(void)
 	struct dirent	*dir;
 	t_list			*head;
 
+	struct stat		foo;
+
 	d = opendir(".");
 	head = NULL;
 	if (d)
@@ -69,13 +73,41 @@ void	read_into_dir(void)
 				head = ft_lstadd_to_head(head, (char *)dir->d_name);
 			if (dir->d_type == 4)
 			{
-				if (ft_strncmp((char *)dir->d_name, ".", 1) != 0)
-					sub((char *)dir->d_name);
-				else
-					printf("%s\n", dir->d_name);
+				//if (ft_strncmp((char *)dir->d_name, ".", 1) != 0)
+				//	sub((char *)dir->d_name);
+				//else
+					//printf("%s\n", dir->d_name);
+				{
+				stat(dir->d_name, &foo);
+			printf("file: %s\n",dir->d_name);
+			printf("time access: %s", ctime(&foo.st_atime));
+			printf("time modified: %s", ctime(&foo.st_mtime));
+			printf("time status: %s", ctime(&foo.st_ctime));
+			printf("uid: %u\n", foo.st_uid);
+			printf("gid: %u\n", foo.st_gid);
+			printf("size: %lld\n", foo.st_size);
+			printf("link: %hu\n", foo.st_nlink);
+			printf("mode: %hu\n", foo.st_mode);
+			printf("\n");
+				}
 			}
 			else
-				printf("%s\n", dir->d_name);
+				//printf("%s\n", dir->d_name);
+			{
+			stat(dir->d_name, &foo);
+			printf("file: %s\n",dir->d_name);
+			printf("time access: %s", ctime(&foo.st_atime));
+			printf("time modified: %s", ctime(&foo.st_mtime));
+			printf("time status: %s", ctime(&foo.st_ctime));
+			printf("uid: %u\n", foo.st_uid);
+			printf("gid: %u\n", foo.st_gid);
+			printf("size: %lld\n", foo.st_size);
+			printf("link: %hu\n", foo.st_nlink);
+			printf("mode: %hu\n", foo.st_mode);
+			printf("\n");
+			//printf("time %ld\n", foo.st_atime);
+			//printf("time %ld\n", foo.st_atime);
+			}
 		}
 
 		closedir(d);

b25dbb230fcde9d83ce20a19ad05b3a9617ca035
Joseph NGOMA <jngoma@e4r22p1.wethinkcode.co.za>
2017-12-04 11:12:50 +0200

    push

--- ls_beta/sort_lst.c
+++ ls_beta/sort_lst.c
@@ -6,33 +6,23 @@
 /*   By: jngoma <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/30 10:09:00 by jngoma            #+#    #+#             */
-/*   Updated: 2017/11/30 10:34:47 by jngoma           ###   ########.fr       */
+/*   Updated: 2017/12/04 10:26:19 by jngoma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	sort_by_name(t_filedata **cur)
+void	sort_by_name(t_filedata **list)
 {
+	t_filedata *cur;
 	t_filedata *prev;
-	t_filedata *next;
-	t_filedata *temp;
 
-	temp = NULL;
-	next = NULL;
-	prev = NULL;
-	printf("\n");
-	while ((*cur))
+	cur = (*list);
+	while (cur)
 	{
-		if (prev && ((*cur)->next))
-		{
-			printf("prev is %s\n", prev->name);
-			printf("cur is %s\n", (*cur)->name);
-			printf("cur next is %s\n", (*cur)->next->name);
-			if(ft_strcmp(((*cur)->name), (*cur)->next->name) > 0)
-				printf("change\n");
-		}
-		prev = (*cur);
-		(*cur) = (*cur)->next;
+		prev = cur;
+		cur = cur->next;
+		printf("prev %s\n", prev->name);
+		printf("cur %s\n", cur->name);
 	}
 }
